6486ca5d5f42228e306b2f9c1c63dc26a0d64f4e
Oceane JAUREGUI <ojauregu@e1r5p14.42.fr>
2021-12-27 19:49:02 +0100

    includes

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,13 +6,15 @@
 /*   By: ojauregu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 17:16:23 by ojauregu          #+#    #+#             */
-/*   Updated: 2021/12/27 19:30:07 by ojauregu         ###   ########.fr       */
+/*   Updated: 2021/12/27 19:47:44 by ojauregu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINITALK_H
 # define MINITALK_H
 # include <signal.h>
 # define BUFFER 2048
+# include <unistd.h>
+# include <stdlib.h>
 
 typedef struct s_minit
 {
